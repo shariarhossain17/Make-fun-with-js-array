fb5c185a5494d7d3880ecde5e12d5631ff9627b0
shariarhossain23e <shariarhossain23@gamil.comm>
2022-06-27 03:43:39 +0600

    array literal

---
+++ array_literal/array_literal.js
@@ -0,0 +1,24 @@
+// array literal
+
+const number = [];
+
+number[0] = 1;
+number[1] = 2;
+number[2] = 3;
+
+number[100] = 1000;
+// console.log(number);
+
+// length - 1
+// length +1
+
+
+
+const names = [
+    "shariar","Niloy","arrjun","jony"
+]
+
+names[names.length] = "Akash"
+console.log(names);
+
+
